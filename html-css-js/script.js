a11f9baaf584b8a001951ffcebe165571b3b68d8
RamirezAgus <rlagustin0@gmail.com>
2025-10-15 15:22:50 -0300

    second class

--- html-css-js/script.js
+++ html-css-js/script.js
@@ -10,8 +10,52 @@ jobsListingSection.addEventListener('click', function(event) {
   }
 })
 
-const filter = document.querySelector('#filter-technology')
+const filter = document.querySelector('#filter-location')
+const mensaje = document.querySelector('#filter-selected-value')
+const jobs = document.querySelectorAll('.job-listing-card')
 
 filter.addEventListener('change', function() {
-    console.log(filter.value)
+
+  const selectedValue = filter.value
+   // console.log(filter.value)
+   if(selectedValue) {
+    mensaje.textContent = `Has seleccionado: ${selectedValue}`
+   } else {
+    mensaje.textContent = ''
+   }
+
+   jobs.forEach(job => {
+   // console.log(job.dataset.modalidad)
+   //const modalidad = job.dataset.modalidad
+   const modalidad = job.getAttribute('data-modalidad')
+
+   if(selectedValue === '' || selectedValue === modalidad) {
+    job.style.display = 'flex'
+   } else {
+    job.style.display = 'none'
+   }
+  })
 });
+
+
+
+/*const searchInput = document.querySelector('#empleos-search-input')
+
+searchInput.addEventListener('input', function() {
+  console.log(searchInput.value)
+});
+
+searchInput.addEventListener('blur', function() {
+  console.log('Se dispara cuando el campo pierde el foco');
+});
+
+const searchForm = document.querySelector('#empleos-search-form')
+
+searchForm.addEventListener('submit', function(event) {
+  event.preventDefault() // Evita el envío del formulario
+  console.log('submit')
+})
+
+document.addEventListener('keydown', function(event) {
+
+})*/
